514e0cdfc654be5ddc1c9ea011e064c21d66a064
Dynarc <bobymaxime@live.fr>
2021-09-24 09:25:51 +0200

    config .htaccess and start router

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+define("URL", str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
+
+if(empty($_GET['page'])){
+
+    require_once 'views/accueil.view.php';
+
+} else{
+
+    $url = explode("/", filter_var($_GET['page']), FILTER_SANITIZE_URL);
+
+    try {
+
+        switch ($url[0]){
+
+            case 'accueil':
+                
+
+            
+
+            default:
+                throw new Exception("La page n'existe pas");
+                break;
+
+        }
+
+    }catch (Exception $e){
+        $test = $e->getMessage();
+        
+    }
+}
