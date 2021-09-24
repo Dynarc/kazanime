0389c630c19868cf4a658f3049bb15ebb3c11edf
Dynarc <bobymaxime@live.fr>
2021-09-24 14:15:09 +0200

    home done (html + css + js)

--- Views/accueil.view.php
+++ Views/accueil.view.php
@@ -26,6 +26,80 @@
 
 </div>
 
+<section class="main-accueil">
+
+    <article class="last-anime-added">
+
+        <h2 class="RO">Derniers animes ajoutés</h2>
+
+        <div class="last-added">
+            <img src="https://picsum.photos/150/150" alt="">
+            <p>ceci est le nom d'un anime</p>
+        </div>
+
+        <div class="last-added">
+            <img src="https://picsum.photos/150/150" alt="">
+            <p>ceci est le nom d'un anime</p>
+        </div>
+
+        <div class="last-added">
+            <img src="https://picsum.photos/150/150" alt="">
+            <p>ceci est le nom d'un anime</p>
+        </div>
+
+        <div class="last-added">
+            <img src="https://picsum.photos/150/150" alt="">
+            <p>ceci est le nom d'un anime</p>
+        </div>
+
+        <a href="">Voir plus <i class="fas fa-plus"></i></a>
+        
+    </article>
+
+    <div class="separate"></div>
+    
+    <article class="last-episode-added">
+
+        <h2 class="RO">Derniers épisodes ajoutés</h2>
+
+        <div class="last-added">
+            <img src="https://picsum.photos/150/150" alt="">
+            <div>
+                <p>ceci est le nom d'un anime</p>
+                <small>episode 45: le nid de coucou</small>
+            </div>
+        </div>
+
+        <div class="last-added">
+            <img src="https://picsum.photos/150/150" alt="">
+            <div>
+                <p>ceci est le nom d'un anime</p>
+                <small>episode 45: le nid de coucou</small>
+            </div>
+        </div>
+
+        <div class="last-added">
+            <img src="https://picsum.photos/150/150" alt="">
+            <div>
+                <p>ceci est le nom d'un anime</p>
+                <small>episode 45: le nid de coucou</small>
+            </div>
+        </div>
+
+        <div class="last-added">
+            <img src="https://picsum.photos/150/150" alt="">
+            <div>
+                <p>ceci est le nom d'un anime</p>
+                <small>episode 45: le nid de coucou</small>
+            </div>
+        </div>
+
+        <a href="">Voir plus <i class="fas fa-plus"></i></a>
+
+    </article>
+
+</section>
+
 <?php
 $titre = "Accueil";
 $content = ob_get_clean();
