86f4f4d9692a92a92ed380402f11fe03be8328e8
Dynarc <bobymaxime@live.fr>
2021-09-24 10:55:29 +0200

    creation of header/footer and css of them

---
+++ Views/template.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="public/css/style.css">
+    <title><?=$titre?></title>
+</head>
+<body>
+
+    <header>
+        <div id="brand">
+            <h1>Kazanime</h1>
+            <a href='https://www.freepik.com/vectors/background'>Background vector created by upklyak - www.freepik.com</a>
+        </div>
+
+        <nav>
+            <ul class="RO">
+
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Liste des animes</a></li>
+                <li><a href="">Proposer un anime</a></li>
+                <li><a href="">Mon compte</a></li>
+                <li><i class="fas fa-search"></i></li>
+
+            </ul>
+        </nav>
+        
+    </header>
+    
+
+    <?= $content ?>
+
+    <footer>
+        <nav>
+            <ul>
+
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Liste des animes</a></li>
+                <li><a href="">Proposer un anime </a></li>
+                <li><a href="">Mon compte</a></li>
+                <li><a href="">A propos</a></li>
+                <li><a href="">Contact</a></li>
+
+            </ul> 
+        </nav>
+    </footer>
+
+</body>
+</html>
