2b9921bf88f2550aa4793c0f153576809f889048
Dynarc <bobymaxime@live.fr>
2021-10-06 13:57:30 +0200

    add database structure (.sql) and add connection to database (model class)

---
+++ kazanime v1.sql
@@ -0,0 +1,291 @@
+create database kazanime CHARACTER SET utf8 COLLATE utf8_general_ci;
+use kazanime;
+
+create table tag(
+	id_tag int auto_increment primary key not null,
+    nom_tag varchar(50)
+);
+
+create table genre(
+	id_genre int auto_increment primary key not null,
+    nom_genre varchar(50)
+);
+
+create table studio(
+	id_studio int auto_increment primary key not null,
+    nom_studio varchar(50)
+);
+
+create table detenir(
+	id_anime int,
+    id_tag int,
+    primary key(id_anime, id_tag)
+);
+
+create table avoir(
+	id_anime int,
+    id_genre int,
+    primary key(id_anime, id_genre)
+);
+
+create table disposer(
+	id_anime int,
+    id_studio int,
+    primary key(id_anime, id_studio)
+);
+
+create table anime(
+	id_anime int auto_increment primary key not null,
+    nom_anime varchar(100),
+    nom_alt_anime text,
+    image_anime varchar(150),
+    image_miniature_anime varchar(150),
+    date_debut_anime date,
+    date_fin_anime date,
+    synopsis_anime text,
+    nombre_episode_anime int,
+    duree_episode_anime int
+);
+
+create table episode(
+	id_episode int auto_increment primary key not null,
+    nom_episode varchar(100),
+    numero_episode float,
+    id_anime int
+);
+
+create table diffuseur(
+	id_diffuseur int auto_increment primary key not null,
+    nom_diffuseur varchar(50),
+    lien_diffuseur varchar(100)
+);
+
+create table diffuser(
+	id_anime int,
+    id_diffuseur int,
+    lien_anime varchar(150),
+    primary key(id_anime, id_diffuseur)
+);
+
+create table retransmettre(
+	id_episode int,
+    id_diffuseur int,
+    lien_episode varchar(150),
+    primary key(id_episode, id_diffuseur)
+);
+
+create table commentaire(
+	id_commentaire int auto_increment primary key not null,
+    contenu_commentaire text,
+    date_commentaire datetime default current_timestamp,
+    id_user int,
+    id_anime int
+);
+
+create table user(
+	id_user int auto_increment primary key not null,
+    pseudo_user varchar(50),
+	mail_user varchar(150),
+    mdp_user varchar(100),
+    mdp_temp_user varchar(100) default null,
+    date_inscription_user datetime default current_timestamp,
+    id_role int
+);
+
+create table voir(
+	id_anime int,
+    id_user int,
+    note_anime int,
+    memo_anime text,
+    nombre_episode_vu int,
+    date_ajout datetime default current_timestamp,
+    primary key(id_anime, id_user)
+);
+
+create table liste(
+	id_liste int auto_increment primary key not null,
+    nom_liste varchar(50),
+    description_liste varchar(150),
+    date_creation_liste varchar(50),
+    id_user int
+);
+
+create table lister(
+	id_anime int,
+    id_liste int,
+	primary key(id_anime, id_liste)
+);
+
+create table partager(
+	id_user int,
+    id_liste int,
+    primary key(id_user, id_liste)
+);
+
+create table proposition(
+	id_proposition int auto_increment primary key not null,
+    nom_proposition varchar(100),
+    date_proposition datetime default current_timestamp,
+    nom_alt_proposition text,
+    nombre_episode_proposition int,
+    duree_episode_proposition int,
+    synopsis_proposition text,
+    studio_proposition varchar(50),
+    date_debut_proposition date,
+    date_fin_proposition date,
+    diffuseur_proposition varchar(150),
+    genre_proposition varchar(200),
+    tag_proposition varchar(200),
+    id_user int,
+    id_statut int
+);
+
+create table statut_proposition(
+	id_statut int auto_increment primary key not null,
+    nom_statut varchar(50)
+);
+
+create table role(
+	id_role int auto_increment primary key not null,
+    nom_role varchar(50)
+);
+
+create table sanction(
+	id_sanction int auto_increment primary key not null,
+    raison_sanction varchar(50),
+    date_sanction datetime default current_timestamp,
+    duree_sanction int,
+    commentaire_sanction text,
+    id_type_sanction int,
+    id_user int
+);
+
+create table type_sanction(
+	id_type_sanction int auto_increment primary key not null,
+    nom_type_sanction varchar(50)
+);
+
+alter table detenir
+add constraint fk_detenir_anime
+foreign key (id_anime)
+references anime(id_anime); 
+
+alter table detenir
+add constraint fk_detenir_tag
+foreign key (id_tag)
+references tag(id_tag);
+
+alter table avoir
+add constraint fk_avoir_anime
+foreign key (id_anime)
+references anime(id_anime);
+
+alter table avoir
+add constraint fk_avoir_genre
+foreign key (id_genre)
+references genre(id_genre);
+
+alter table disposer
+add constraint fk_disposer_anime
+foreign key (id_anime)
+references anime(id_anime);
+
+alter table disposer
+add constraint fk_disposer_studio
+foreign key (id_studio)
+references studio(id_studio);
+
+alter table episode
+add constraint fk_episode_anime
+foreign key (id_anime)
+references anime(id_anime);
+
+alter table retransmettre
+add constraint fk_retransmettre_episode
+foreign key (id_episode)
+references episode(id_episode);
+
+alter table retransmettre
+add constraint fk_retransmettre_diffuseur
+foreign key (id_diffuseur)
+references diffuseur(id_diffuseur);
+
+alter table diffuser
+add constraint fk_diffuser_anime
+foreign key (id_anime)
+references anime(id_anime);
+
+alter table diffuser
+add constraint fk_diffuser_diffuseur
+foreign key (id_diffuseur)
+references diffuseur(id_diffuseur);
+
+alter table commentaire
+add constraint fk_commentaire_user
+foreign key (id_user)
+references user(id_user);
+
+alter table commentaire
+add constraint fk_commentaire_anime
+foreign key (id_anime)
+references anime(id_anime);
+
+alter table voir
+add constraint fk_voir_anime
+foreign key (id_anime)
+references anime(id_anime);
+
+alter table voir
+add constraint fk_voir_user
+foreign key (id_user)
+references user(id_user);
+
+alter table user
+add constraint fk_user_role
+foreign key (id_role)
+references role(id_role);
+
+alter table lister
+add constraint fk_lister_anime
+foreign key (id_anime)
+references anime(id_anime);
+
+alter table lister
+add constraint fk_lister_liste
+foreign key (id_liste)
+references liste(id_liste);
+
+alter table liste
+add constraint fk_liste_user
+foreign key (id_user)
+references user(id_user);
+
+alter table partager
+add constraint fk_partager_user
+foreign key (id_user)
+references user(id_user);
+
+alter table partager
+add constraint fk_partager_liste
+foreign key (id_liste)
+references liste(id_liste);
+
+alter table proposition
+add constraint fk_proposition_user
+foreign key (id_user)
+references user(id_user);
+
+alter table proposition
+add constraint fk_proposition_statut
+foreign key (id_statut)
+references statut(id_statut);
+
+alter table sanction
+add constraint fk_sanction_type_sanction
+foreign key (id_type_sanction)
+references type_sanction(type_sanction);
+
+alter table sanction
+add constraint fk_sanction_user
+foreign key (id_user)
+references user(id_user);
