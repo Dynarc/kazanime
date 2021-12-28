3991b66581c3e2a74e77fcf979d1bcf1aa79fc15
Dynarc <bobymaxime@live.fr>
2021-12-28 15:39:11 +0100

    secure display

--- Controllers/GlobalController.controller.php
+++ Controllers/GlobalController.controller.php
@@ -14,6 +14,6 @@ abstract class GlobalController{
     public static function sendJson($data){
         header("Access-Control-Allow-Origin: * ");
         header("Content-Type: application/json");
-        echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        echo htmlspecialchars(json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
     }
 }
