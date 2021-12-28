84b68c7e8ebb708b71df6141c377c8237cedc6fe
Dynarc <bobymaxime@live.fr>
2021-12-28 15:40:18 +0100

    add account view for admin

---
+++ Views/adminAccount.view.php
@@ -0,0 +1,38 @@
+<?php ob_start() ?>
+
+    <h2 class="crud-title">Liste des comptes</h2>
+    <section class="crud-list">
+        <article>
+            <?php if(!empty($accounts)) foreach ($accounts as $account) { ?>
+                <div>
+                    <p><?=$account->getPseudo()?></p>
+                    <p><?=$account->getEmail()?></p>
+                    <p><?=date("d-m-Y",strtotime($account->getDate_inscription()))?></p>
+                    <p><?= $account->getRole() == 1 ? 'admin' : 'user' ?></p>
+                    <div>
+                        <button value="<?=URL?>admin/account/history/<?=$account->getId()?>">Historique</button>
+                        <button>
+                            <a href="<?=URL?>admin/account/sanction/<?=$account->getId()?>">Sanctionner</a>
+                        </button>
+                    </div>
+                </div>
+            <?php } ?>
+
+        </article>
+    </section>
+
+    <?php
+        if (!empty($_SESSION['alert'])) :
+    ?>
+        <div class="message <?= $_SESSION['alert']['type'] ?>">
+            <?= $_SESSION['alert']['msg'] ?>
+        </div>
+    <?php
+        endif
+    ?>
+
+
+<?php
+$titre = "Admin account";
+$content = ob_get_clean();
+require_once "template.php";
