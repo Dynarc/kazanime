065a0dec2a635dc153475992eedc3b7c368a6c34
Dynarc <bobymaxime@live.fr>
2021-12-28 14:44:13 +0100

    update form and add error message

--- Views/connexion.view.php
+++ Views/connexion.view.php
@@ -4,13 +4,13 @@
     
     <h1 class="RO">Connexion</h1>
     
-    <form action="">
+    <form action="<?=URL?>connexion/connect" method="POST">
         
         <label for="pseudo">Pseudo</label>
-        <input type="text" name="pseudo">
+        <input type="text" name="pseudo" required>
 
         <label for="password">Mot de passe</label>
-        <input type="password" name="password">
+        <input type="password" name="password" required>
 
         <input type="submit" value="Se connecter" class="RO">
 
@@ -19,6 +19,16 @@
     <u><a href="">Mot de passe oublié ?</a></u>
 
     <p>Pas encore de compte ? <u><a href="<?=URL?>inscription">S'inscrire</a></u></p>
+
+    <?php
+        if (!empty($_SESSION['alert'])) :
+    ?>
+        <div class="message <?= $_SESSION['alert']['type'] ?>">
+            <?= $_SESSION['alert']['msg'] ?>
+        </div>
+    <?php
+        endif
+    ?>
 
 </section>
 
