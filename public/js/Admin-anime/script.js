f649152aceae1a9a2737cfd5f6756d427db573e0
Dynarc <bobymaxime@live.fr>
2022-01-13 11:08:28 +0100

    add fetch update tag (WIP)

--- public/js/Admin-anime/script.js
+++ public/js/Admin-anime/script.js
@@ -1,8 +1,31 @@
 let showAdd = document.querySelector('.crud-add');
 let formAdd = document.querySelector('.form-crud-add');
+let formTag = document.querySelector('.admin-display-detail>article>form');
 
 function showAddForm(){
     formAdd.style.display = formAdd.style.display == "flex" ? "none" : "flex";
 }
 
-showAdd.addEventListener('click',showAddForm);
+function noSub(e) {
+    e.preventDefault();
+}
+
+function updateTag() {
+    let form = new FormData(formTag);
+
+    fetch(formTag.attributes.action.value, {
+        method: "POST",
+        body: form,
+    })
+    .then(response => response.text())
+    .then(response => document.querySelector('p').innerHTML = response)
+    .catch(err => console.error(err));
+}
+
+if(showAdd != undefined) {
+    showAdd.addEventListener('click',showAddForm);
+}
+if(formTag != undefined) {
+    formTag.addEventListener("submit", noSub);
+    formTag.lastElementChild.addEventListener('click', updateTag);
+}
