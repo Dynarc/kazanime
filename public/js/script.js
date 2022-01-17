ef3ef2e03cb30d216ec31a3bb21ddb3ec510bca8
Dynarc <bobymaxime@live.fr>
2022-01-17 10:31:17 +0100

    add js for alert messages

--- public/js/script.js
+++ public/js/script.js
@@ -12,6 +12,33 @@ function hideSearchBar(){
     search.style.display = 'none';
 }
 
+
+
+function displayMessage() {
+    let messages = document.querySelector('.message');
+    if(messages) {
+        for (let i = 0; i < messages.children.length; i++) {
+            let message = messages.children[i];
+            message.innerHTML = '<progress max="200" value="0"></progress>' + message.innerHTML;
+            barProgression(message.firstChild, 30);
+            message.lastChild.lastChild.addEventListener('click', () => message.remove());
+        }
+    }
+}
+
+function barProgression(element, time) {
+    if(element.value < 200) {
+        element.value += 1;
+        setTimeout(() => barProgression(element, time),time);
+    } else {
+        clearTimeout(barProgression);
+        element.parentElement.remove();
+    }
+}
+
 // event nav's search bar
 openSearch.addEventListener('click', displaySearchBar);
 closeSearch.addEventListener('click', hideSearchBar);
+
+// Display error or succes messages
+displayMessage();
