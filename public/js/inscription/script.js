bacabc7dfe88cbbfa6161acf34829b4be0167ffe
Dynarc <bobymaxime@live.fr>
2022-01-21 10:58:45 +0100

    fix: change function operation

--- public/js/inscription/script.js
+++ public/js/inscription/script.js
@@ -1,13 +1,14 @@
 let body = document.querySelector('body');
-let section = document.querySelector('.login');
+let footer = document.querySelector('footer');
 
 
 function changeBackground(){
-    body.classList += " newBackground";
-    test = document.createElement('a');
-    test.innerHTML = '<small>Fond vecteur créé par brgfx - fr.freepik.com</small>';
-    test.setAttribute('href','https://fr.freepik.com/vecteurs/fond')
-    section.nextElementSibling.prepend(test);
+    body.classList.add("newBackground");
+    link = document.createElement('a');
+    link.innerHTML = '<small>Fond vecteur créé par brgfx - fr.freepik.com</small>';
+    link.setAttribute('href','https://fr.freepik.com/vecteurs/fond')
+    link.setAttribute('target','_blank')
+    footer.prepend(link);
 }
 
 changeBackground();
