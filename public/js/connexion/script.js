11163c7ca3afac885df294f6f9adc8f82340afa5
Dynarc <bobymaxime@live.fr>
2022-01-21 16:14:23 +0100

    update js

--- public/js/connexion/script.js
+++ public/js/connexion/script.js
@@ -1,7 +1,14 @@
 let body = document.querySelector('body');
+let footer = document.querySelector('footer');
+
 
 function changeBackground(){
-    body.classList += " newBackground";
+    body.classList.add("newBackground");
+    link = document.createElement('a');
+    link.innerHTML = '<small>Fond vecteur créé par brgfx - fr.freepik.com</small>';
+    link.setAttribute('href','https://fr.freepik.com/vecteurs/fond')
+    link.setAttribute('target','_blank')
+    footer.prepend(link);
 }
 
 changeBackground();
